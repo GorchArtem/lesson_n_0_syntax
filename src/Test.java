99c43eb1ccf35d99ef0e87d6a3f344356d34a762
Горчаков Артемий <adamgenesis@MacBook-Pro-Gorcakov.local>
2025-09-01 16:01:21 +0500

    add first examples

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
-
 public class Test {
+
+    // Целочисленные типы
+    int num = 1000123123; //32 bit
+    long numLong = 1000012312123112312L;// 64 bit
+    byte numByte = 127;// 8 bit
+    short numShort = 32767; //16 bit
+
+    // числа с плавающей точкой
+    double numDouble = 3.1415926; // 64 bit
+    float numFloat = 3.1415926f;// 32bit
+
+
+    // символьные
+    char symbolChar = 'H'; // 16bit
+
+    // логический
+    boolean bool = false;
+
+    /**
+     *
+
+     char переменная
+    |H|
+
+    ---------
+
+     String переменная
+    |H|e | l|l |o |
+
+
+    **/
+
+
+    // Ссылочные типы данных
+    String str = "Hello World!";
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public void test1() {
+        System.out.println(num);
+
+
+    }
+
 }
