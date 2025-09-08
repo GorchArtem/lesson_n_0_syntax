d35d786ea14a1469baeb27598b6e7143dfda1075
NovruzSalakhov <salakhov-novruz@mail,ru>
2025-09-08 14:30:15 +0300

    Add new class

---
+++ src/Employee.java
@@ -0,0 +1,25 @@
+public class Employee {
+    String name;
+    String surname;
+    String position;
+    int age;
+
+    public static void main(String[] args) {
+
+        String name1 = "Adam";
+        String surname1 = "Smith";
+        int age1 = 45;
+        String position1 = "Administrtor";
+        String name2 = "Sara";
+        String surname2 = "Johnson";
+        int age2 = 39;
+        String position2 = "Сashier";
+        String name3 = "Tim";
+        String surname3 = "Prett";
+        int age3 = 51;
+        String position3 = "Loader";
+
+        System.out.println(name1 + surname2);
+    }
+
+}
