2922b4cc687b3f968e80730fe2461254b05da8ae
Горчаков Артемий <adamgenesis@MacBook-Pro-2.local>
2025-10-07 20:13:14 +0500

    ArrayList examples

---
+++ src/lesson_arrays/ArraysInstruments.java
@@ -0,0 +1,24 @@
+package lesson_arrays;
+
+public class ArraysInstruments {
+
+    public void replace(int[] a){
+
+    }
+
+    public void delete(){
+
+    }
+
+    public void add(){
+
+    }
+
+    public void get(){
+
+    }
+
+    public void replace(){
+
+    }
+}
--- src/lesson_arrays/ArraysMain.java
+++ src/lesson_arrays/ArraysMain.java
@@ -1,5 +1,8 @@
 package lesson_arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArraysMain {
 
     static int lengthForArrays = 20;
@@ -28,28 +31,53 @@ public class ArraysMain {
         transactionsDB.historyTransactionsIDs[4] = 231;
         transactionsDB.historyTransactionsIDs[5] = 4234;
 
+        String str = "Hello"; // str = ссылка на место в памяти где лежит строка "Hello"  string-pool
+
+        String str2 = "Hello";
+
+
+        int[] arrayInt = new int[30];
+        arrayInt[0] = 1223;
+
+
+        ArrayList<Integer> arrayList = new ArrayList<>();  // diamond operator <>
+
+
+        arrayList.add(2123); // int[] arrayList = new int [];
+        arrayList.add(234); // arrayList[1] = 234;
+        arrayList.add(34234); // arrayList[2] = 34234;
+
+
+        for (int i = 0; i < arrayList.size() - 1; i++) {
+            arrayList.add(i);
+
+            System.out.println(arrayList.get(i));
+        }
+
+
+
 
 
         // Цикл
-        for (int count = 0; count <= transactionsDB.historyTransactionsIDs.length - 1; count++) { // transactionsDB.historyTransactionsIDs.length - 1 = 19
-
-            // Заполняем массив значением из переменной i
-            transactionsDB.historyTransactionsIDs[count] = count + 2;
-
-
-            // Выводим значения из текущей ячейки массива
-            System.out.println(transactionsDB.historyTransactionsIDs[count]);
-
-
-        }
-
-        //breakpoint - бряки
-
+//        for (int count = 0; count <= transactionsDB.historyTransactionsIDs.length - 1; count++) { // transactionsDB.historyTransactionsIDs.length - 1 = 19
+//
+//            // Заполняем массив значением из переменной i
+//            transactionsDB.historyTransactionsIDs[count] = count + 2;
+//
+//
+//            // Выводим значения из текущей ячейки массива
+//            System.out.println(transactionsDB.historyTransactionsIDs[count]);
+//
+//
+//        }
+//
+//        //breakpoint - бряки
+//
     }
 
-    static void printArray(int[] array) {
-
-    }
+//    static void printArray(int[] array) {
+//
+//    }
 
 
 }
